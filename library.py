baf363253ce658ede401f91becb562e808814665
Daria <dashacramb@mail.ru>
2026-04-18 20:38:33 +0300

    6

--- library.py
+++ library.py
@@ -29,4 +29,24 @@ result = list(map(mapper, filtered))
 sorted_items = sorted(items, key=lambda it: it[1].lower())
 l = list(map(mapper, sorted_items))
 
+if action == "filter":
+    filtered = filter(lambda item: item[1] == arg, items)
+    result = list(map(lambda item: f"{item[0]} — {item[1]}", filtered))
+    for line in result:
+        print(line)
+elif action == "sort":
+    if arg == "author":
+        sorted_items = sorted(items, key=lambda it: it[1].lower())
+    elif arg == "book":
+        sorted_items = sorted(items, key=lambda it: it[0].lower())
+    else:
+        print('Для sort используйте arg: "author" или "book"')
+        sys.exit(1)
+    result = list(map(lambda item: f"{item[0]} — {item[1]}", sorted_items))
+    for line in result:
+        print(line)
+
+else:
+    print('Использование: python library.py <action> <arg> action: filter|sort arg: для filter — имя автора; для sort — "author" или "book"')
+
 
